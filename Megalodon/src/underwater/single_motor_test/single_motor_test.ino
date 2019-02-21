8e17fbb3e5118b5d78a455a68e30d51dcbc20239
tedklin <tedklin@gmail.com>
2019-02-21 11:45:32 -0800

    single motor constant throttle test

---
+++ Megalodon/src/underwater/single_motor_test/single_motor_test.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+byte servoPin = 9;
+float input
+Servo motor1;
+
+// set power from -1.0 to 1.0
+void setThrottle(float motorThrottle) {
+  float input = motorThrottle * 400 + 1500;
+  motor1.writeMicroseconds(input);
+  Serial.print("Motor 1 Throttle: ");
+  Serial.print(motorThrottle);
+  Serial.print(" || Motor 1 input: ");
+  Serial.print(input);
+  Serial.println();
+
+void setup() {
+  Serial.begin(9600);
+  
+  motor1.attach(servoPin);
+  setThrottle(0);
+  delay(7000);
+}
+
+void loop() {
+//  if (Serial.read() != input) {
+//    
+//  }
+  setThrottle(0.3);
+}
+
