3a872aca6085d9e614faa9d00f102be4d18dbcd2
tedklin <tedklin@gmail.com>
2019-03-12 13:03:23 -0700

    add variables

--- Megalodon/src/underwater/navigation/navigation.ino
+++ Megalodon/src/underwater/navigation/navigation.ino
@@ -7,6 +7,47 @@ Servo verticalFrontLeftMotor;
 Servo verticalBackRightMotor;
 Servo verticalBackLeftMotor;
 
+const float kYawP = 0;
+const float kYawI = 0;
+const float kYawD = 0;
+const float kPitchP = 0;
+const float kPitchI = 0;
+const float kPitchD = 0;
+const float kRollP = 0;
+const float kRollI = 0;
+const float kRollD = 0;
+
+const float kXP = 0;
+const float kXI = 0;
+const float kXD = 0;
+const float kYP = 0;
+const float kYI = 0;
+const float kYD = 0;
+const float kZP = 0;
+const float kZI = 0;
+const float kZD = 0;
+
+float kDesiredYaw = 0;
+float kDesiredPitch = 0;
+float kDesiredRoll = 0;
+float kDesiredX = 0;
+float kDesiredY = 0;
+float kDesiredZ = 0;
+
+float kHorizontalRightPower = 0;
+float kHorizontalLeftPower = 0;
+float kVerticalFrontRightPower = 0;
+float kVerticalFrontLeftPower = 0;
+float kVerticalBackRightPower = 0;
+float kVerticalBackLeftPower = 0;
+
+float kYawControlOutput = 0;
+float kRollControlOutput = 0;
+float kPitchControlOutput = 0;
+float kXControlOutput = 0;
+float kYControlOutput = 0;
+float kZControlOutput = 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -23,6 +64,8 @@ void setup() {
 }
 
 void loop() {
+
+  updateMotorInput();
 }
 
 /**
@@ -38,16 +81,3 @@ void setThrottle(Servo motor, float throttle) {
   Serial.print(input);
   Serial.println();
 }
-
-void stopAll() {
-  setThrottle(horizontalRightMotor, 0.0);
-  setThrottle(horizontalLeftMotor, 0.0);
-  setThrottle(verticalFrontRightMotor, 0.0);
-  setThrottle(verticalFrontLeftMotor, 0.0);
-  setThrottle(verticalBackRightMotor, 0.0);
-  setThrottle(verticalBackLeftMotor, 0.0);
-}
-
-// TODO: write function that takes pose and does internal PID
-void 
-
