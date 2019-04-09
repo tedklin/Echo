991e5f6f5746a6848e8b3f9f8882072fdf30c589
tedklin <tedklin@gmail.com>
2019-04-09 13:16:38 -0700

    fix descriptors

--- Megalodon/src/underwater/imu_test/imu_test.ino
+++ Megalodon/src/underwater/imu_test/imu_test.ino
@@ -102,25 +102,25 @@ void loop(void)
   // - VECTOR_GRAVITY       - m/s^2
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-//  /* Display the floating point data */
-//  Serial.print("Roll: ");
-//  Serial.print(euler.x());
-//  Serial.print(" Pitch: ");
-//  Serial.print(euler.y());
-//  Serial.print(" Yaw: ");
-//  Serial.print(euler.z());
+  /* Display the floating point data */
+  Serial.print("Yaw: ");
+  Serial.print(euler.x());
+  Serial.print(" Roll: ");
+  Serial.print(euler.y());
+  Serial.print(" Pitch: ");
+  Serial.print(euler.z());
+  Serial.println("\t\t");
+
+//  imu::Vector<3> linearAccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+//  float linearAccelX = linearAccel.x();
+//  float linearAccelY = linearAccel.y();
+//  float linearAccelZ = linearAccel.z();
+//
+//  currentMillis = millis() / 1000.0;
+//  float timeDifference = currentMillis - previousMillis;
+//  previousMillis = currentMillis;
+//  Serial.print(timeDifference);
 //  Serial.print("\t\t");
-
-  imu::Vector<3> linearAccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  float linearAccelX = linearAccel.x();
-  float linearAccelY = linearAccel.y();
-  float linearAccelZ = linearAccel.z();
-
-  currentMillis = millis() / 1000.0;
-  float timeDifference = currentMillis - previousMillis;
-  previousMillis = currentMillis;
-  Serial.print(timeDifference);
-  Serial.print("\t\t");
 
 //  if (linearAccelX < 0) {
 //    x += -linearAccelX * linearAccelX * timeDifference * timeDifference;
@@ -138,69 +138,69 @@ void loop(void)
 //    z += linearAccelZ * linearAccelZ * timeDifference * timeDifference;
 //  }
 
-  if (abs(linearAccelX) < 0.2) {
-    linearAccelX = 0;
-  }
-  if (abs(linearAccelY) < 0.2) {
-    linearAccelY = 0;
-  }
-  if (abs(linearAccelZ) < 0.2) {
-    linearAccelZ = 0;
-  }
-
-  if (abs(linearAccelX) < 0.08) {
-    vel_x = 0;
-  }
-  if (abs(linearAccelY) < 0.08) {
-    vel_y = 0;
-  }
-  if (abs(linearAccelZ) < 0.08) {
-    vel_z = 0;
-  }
-
-  if (isIntermediateStep == false) {
-    innerCurrentMillis = millis() / 1000.0;
-    x += 0.5 * (vel_x + lastVelX) * (innerCurrentMillis - innerPreviousMillis);
-    y += 0.5 * (vel_y + lastVelY) * (innerCurrentMillis - innerPreviousMillis);
-    z += 0.5 * (vel_z + lastVelZ) * (innerCurrentMillis - innerPreviousMillis);
-   
-    lastVelX = vel_x;
-    lastVelY = vel_y;
-    lastVelZ = vel_z;
-
-    innerPreviousMillis = innerCurrentMillis;
-    isIntermediateStep = true;
-  } else {
-    isIntermediateStep = false;
-  }
+//  if (abs(linearAccelX) < 0.2) {
+//    linearAccelX = 0;
+//  }
+//  if (abs(linearAccelY) < 0.2) {
+//    linearAccelY = 0;
+//  }
+//  if (abs(linearAccelZ) < 0.2) {
+//    linearAccelZ = 0;
+//  }
+//
+//  if (abs(linearAccelX) < 0.08) {
+//    vel_x = 0;
+//  }
+//  if (abs(linearAccelY) < 0.08) {
+//    vel_y = 0;
+//  }
+//  if (abs(linearAccelZ) < 0.08) {
+//    vel_z = 0;
+//  }
+//
+//  if (isIntermediateStep == false) {
+//    innerCurrentMillis = millis() / 1000.0;
+//    x += 0.5 * (vel_x + lastVelX) * (innerCurrentMillis - innerPreviousMillis);
+//    y += 0.5 * (vel_y + lastVelY) * (innerCurrentMillis - innerPreviousMillis);
+//    z += 0.5 * (vel_z + lastVelZ) * (innerCurrentMillis - innerPreviousMillis);
+//   
+//    lastVelX = vel_x;
+//    lastVelY = vel_y;
+//    lastVelZ = vel_z;
+//
+//    innerPreviousMillis = innerCurrentMillis;
+//    isIntermediateStep = true;
+//  } else {
+//    isIntermediateStep = false;
+//  }
 
 //  x += 0.5 * linearAccelX * timeDifference * timeDifference;
 //  y += 0.5 * linearAccelY * timeDifference * timeDifference;
 //  z += 0.5 * linearAccelZ * timeDifference * timeDifference;
   
-  vel_x += 0.5 * (linearAccelX + lastLinearAccelX) * timeDifference;
-  vel_y += 0.5 * (linearAccelY + lastLinearAccelY) * timeDifference;
-  vel_z += 0.5 * (linearAccelZ + lastLinearAccelZ) * timeDifference;
-
-  lastLinearAccelX = linearAccelX;
-  lastLinearAccelY = linearAccelY;
-  lastLinearAccelZ = linearAccelZ;
-  
-  Serial.print("Accel X: ");
-  Serial.print(linearAccelX);
-  Serial.print(" Accel Y: ");
-  Serial.print(linearAccelY);
-  Serial.print(" Accel Z: ");
-  Serial.print(linearAccelZ);
-  Serial.print("\t");
-  
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(x * 100);
-  Serial.print(" Y: ");
-  Serial.print(y * 100);
-  Serial.print(" Z: ");
-  Serial.println(z * 100);
+//  vel_x += 0.5 * (linearAccelX + lastLinearAccelX) * timeDifference;
+//  vel_y += 0.5 * (linearAccelY + lastLinearAccelY) * timeDifference;
+//  vel_z += 0.5 * (linearAccelZ + lastLinearAccelZ) * timeDifference;
+//
+//  lastLinearAccelX = linearAccelX;
+//  lastLinearAccelY = linearAccelY;
+//  lastLinearAccelZ = linearAccelZ;
+//  
+//  Serial.print("Accel X: ");
+//  Serial.print(linearAccelX);
+//  Serial.print(" Accel Y: ");
+//  Serial.print(linearAccelY);
+//  Serial.print(" Accel Z: ");
+//  Serial.print(linearAccelZ);
+//  Serial.print("\t");
+//  
+//  /* Display the floating point data */
+//  Serial.print("X: ");
+//  Serial.print(x * 100);
+//  Serial.print(" Y: ");
+//  Serial.print(y * 100);
+//  Serial.print(" Z: ");
+//  Serial.println(z * 100);
 //  Serial.print("\t\t");
 
 //  // Quaternion data
